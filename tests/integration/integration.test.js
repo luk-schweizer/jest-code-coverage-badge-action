830e9508f31db0b9f93c6ac7344063b2cf9e3ec4
Lucas Schweizer <lucas.sch@gmail.com>
2021-03-22 19:12:57 -0300

    Extending the timeout in test to avoid false negatives due to external services issues

--- tests/integration/integration.test.js
+++ tests/integration/integration.test.js
@@ -1,11 +1,15 @@
 const retry = require('p-retry');
 const fetch = require('node-fetch');
 
+beforeEach(async () => {
+  jest.setTimeout(10000);
+});
+
 test('jest-code-coverage-badge-action output should return an svg badge with label coverage and value 100% when input label is coverage, coverage of test are 100% and key url is not provided', async () => {
   const outputUrl = process.env.BADGE_OUTPUT_WITH_URL_NOT_PROVIDED;
   console.log(`Url ${outputUrl}`);
 
-  const response = await retry(() => fetch(outputUrl, {method: 'get', timeout: 1000}), {retries: 4});
+  const response = await retry(() => fetch(outputUrl, {method: 'get', timeout: 2000}), {retries: 4});
   const svg = await response.text();
 
   expect(svg).toMatch(/^<svg/);
@@ -17,7 +21,7 @@ test('jest-code-coverage-badge-action output should return an svg badge with lab
   const outputUrl = process.env.BADGE_OUTPUT_WITH_URL_PROVIDED;
   console.log(`Url ${outputUrl}`);
 
-  const response = await retry(() => fetch(outputUrl, {method: 'get', timeout: 1000}), {retries: 4});
+  const response = await retry(() => fetch(outputUrl, {method: 'get', timeout: 2000}), {retries: 4});
   const svg = await response.text();
 
   expect(svg).toMatch(/^<svg/);
